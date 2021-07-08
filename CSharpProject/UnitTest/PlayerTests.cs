468a9ee5ac2cddea6901495b10d34b8e7e775aac
Melissa Magistri <melissa.magistri@studio.unibo.it>
2021-07-08 15:47:53 +0200

    Create the tests for the Player

---
+++ CSharpProject/UnitTest/PlayerTests.cs
@@ -0,0 +1,40 @@
+﻿using MagistriProject;
+using GrandiProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest
+{
+  
+    [TestClass]
+    class PlayerTests
+    {
+        [TestMethod]
+        public void IsPlayerAlive()
+        {
+            Player player = new Player1(SpecificEntityType.PLAYER_1, 0.0, 0.0 );
+            Assert.IsTrue(player.IsAlive());
+        }
+
+        [TestMethod]
+        public void IsPlayerDead()
+        {
+            Player player = new Player1(SpecificEntityType.PLAYER_1, 0.0, 0.0);
+            while (player.GetHits() <= player.GetMaxHits())
+            {
+                player.IncHits();
+            }
+            Assert.IsFalse(player.IsAlive());
+        }
+
+        [TestMethod]
+        public void IsAlienMovingCorrectly()
+        {
+            Player player = new Player1(SpecificEntityType.PLAYER_1, 0.0, 0.0);
+            player.UpdateEntityPosition(GameEvent.RIGHT, 2);
+            Assert.IsTrue(player.GetPos().Equals(
+                new Pair<double, double>(0.0 + player.GetMuX(), player.GetY())));
+        }
+
+    }
+}
